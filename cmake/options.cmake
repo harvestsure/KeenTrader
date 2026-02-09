9b83dcf033ce0a78bd55dbb431ca61dfd118f495
harvestsure <harvestsure@gmail.com>
2026-02-09 16:28:48 +0800

    Revert to the old version

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -34,21 +34,4 @@ if(MSVC)
 
     # Turn off CRT warnings:
     add_compile_definitions(_CRT_SECURE_NO_WARNINGS)
-    # add_compile_options(/W4 /WX)
-else()
-    # Enable strict warnings but allow issues common in third-party libraries
-    # add_compile_options(
-    #     -Wall -Wextra -Wpedantic -Werror
-    #     -Wshadow                           # Warn about variable shadowing
-    #     -Wconversion                       # Warn about implicit type conversions
-    #     -Wno-error=unused-parameter
-    #     -Wno-error=unused-variable
-    #     -Wno-error=conversion              # Type conversion warnings from third-party libs
-    #     -Wno-error=shadow                  # Variable shadowing in third-party libs
-    # )
-    
-    # # Clang-specific options
-    # if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    #     add_compile_options(-Wno-error=null-pointer-subtraction)
-    # endif()
 endif()
